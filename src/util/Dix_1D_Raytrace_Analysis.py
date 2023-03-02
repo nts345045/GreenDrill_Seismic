b1cc97abca138e7687c62978d0710f0deee284ad
Nate Stevens <33462727+nts345045@users.noreply.github.com>
2023-03-02 14:25:17 -0500

    Update Dix_1D_Raytrace_Analysis.py

--- src/util/Dix_1D_Raytrace_Analysis.py
+++ src/util/Dix_1D_Raytrace_Analysis.py
@@ -73,18 +73,35 @@ def calc_Vrms_cont(zz,VV):
 
 ### WHB PROFILE RESAMPLING
 
-def resample_WHB(Z,uD,method='incremental increase',scalar=1.3):
+def resample_WHB(Z,uD,method='incremental decrease',scalar=1.3):
+	"""
+	Resample Wiechert-Herglotz-Bateman vertical slowness profile in 
+	a systematic way
 
-	if method.lower() == 'incremental increase':
+	:: INPUTS ::
+	:param Z: depth points in meters
+	:param uD: slowness points in msec/m
+	:param method: how to conduct resampling
+					'incremental increase' - bin by a maximum change in slowness value
+	:param scalar: scalar value to use with specified method
+
+	:: OUTPUTS ::
+	:return Zt: interval top-depth points for new model in meters
+	:return Zb: interval bottom-depth points for new model in meters
+	:return uRMS: RMS interval slowness values for new model in msec/m
+
+	"""
+
+	if method.lower() == 'incremental decrease':
 		# Get surface coordinates
 		u0 = uD[0]; z0 = 0.
 		# Get interval where ui <= u0*scalar
-		I1 = uD <= u0*scalar; z1 = Z[I1][-1]; u1 = uD[I1][-1]
+		I1 = uD <= u0/scalar; z1 = Z[I1][-1]; u1 = uD[I1][-1]
 		# Create holders for resampled slowness profile
-		u_int = []; z_top_int = []; z_bot_int = [];
+		uRMS = []; Zt = []; Zb = [];
 		# Calculate first interval
 		ui = calc_Vrms_cont(Z[I1],uD[I1]**-1)**-1
-		z_top_int.append(z0); z_bot_int.append(z1)
+		Zt.append(z0); Zb.append(z1)
 		# While there is still a usable profile
 		while z1 < Z:
 			# Advance bounds and calculate new index
@@ -94,26 +111,26 @@ def resample_WHB(Z,uD,method='incremental increase',scalar=1.3):
 			# Calculate RMS slowness within new layer
 			ui = calc_Vrms_cont(Z[I1],uD[I1]**-1)**-1
 			# Save entries
-			u_int.append(ui)
-			z_top_int.append(z0); z_bot_int.append(z1)
+			uRMS.append(ui)
+			Zt.append(z0); Zb.append(z1)
 
 	# elif method.lower() == 'uniform'
-	# 	u_int = [uD[0]]; z_top_int = [0]; z_bot_int = [scalar]
+	# 	uRMS = [uD[0]]; Zt = [0]; Zb = [scalar]
 
-	mod_out = {'Ztop':z_top_int,'Zbot':z_bot_int,'uRMS':u_int}
-	return mod_out
+	# mod_out = {'Ztop':z_top_int,'Zbot':Zb,'uRMS':uRMS}
+	return Zt,Zb,uRMS
 
 
 ### 1D RAY TRACING ###
-def generate_layercake_slow(Sv=[1/2500,1/3750,1/5000],Zv=[0,10,490,4000]):
+def generate_layercake_slow(Uv=[1/2500,1/3850],Zv=[0,10,4000]):
 	"""
 	Generate a 1-D layercake seismic velocity model with homogeneous interval 
 	slownesses bounded by Zv horizons. For use with pyrocko.cake
 
-	Note that Zv must have 1 additional entry compared to Sv
+	Note that Zv must have 1 additional entry compared to Uv
 
 	:: INPUTS ::
-	:param Sv: array-like set of slownesses in sec/m
+	:param Uv: array-like set of slownesses in sec/m
 	:param Zv: array-like set of horizon depths with z positive down
 
 	:: OUTPUT ::
@@ -121,16 +138,16 @@ def generate_layercake_slow(Sv=[1/2500,1/3750,1/5000],Zv=[0,10,490,4000]):
 	"""
 
 	model = cake.LayeredModel()
-	for i_,S_ in enumerate(Sv):
+	for i_,S_ in enumerate(Uv):
 		mi = cake.Material(vp=S_**-1)
 		lay = cake.HomogeneousLayer(Zv[i_],Zv[i_+1],mi)
 		model.append(lay)
-		if i_ < len(Sv) - 1:
-			mj = cake.Material(vp=Sv[i_+1]**-1)
-			ifce = cake.Interface(Zv[i_+1],mi,mj,)
-		elif i_ == len(Sv) - 1:
-			mj = cake.Material(vp=1.01*Sv[-1]**-1)
-			ifce = cake.Interface(Zv[-1],mi,mj)
+		# if i_ < len(Uv) - 1:
+		# 	mj = cake.Material(vp=Uv[i_+1]**-1)
+		# 	ifce = cake.Interface(Zv[i_+1],mi,mj,)
+		# elif i_ == len(Uv) - 1:
+		# 	mj = cake.Material(vp=1.01*Uv[-1]**-1)
+		# 	ifce = cake.Interface(Zv[-1],mi,mj)
 	return model
 
 def generate_layercake_vel(Vv=[2500,3850],Zv=[0,40,4000]):
@@ -168,14 +185,25 @@ def WHB2CakeMod(Uv,Zv,method='incremental increase',scalar=1.3):
 
 	return model
 
-def add_halfspace(CakeMod,Vn,Hn=4000):
+def add_halfspace(CakeMod,Vn,Hn=4000,verb=False):
 	"""
-	Add a very thick bottom layer to an exsiting 
+	Add a very thick bottom layer to a copy of an exsiting pyrocko.Cake.LayeredModel
 	"""
-	NewMod = deepcopy
-
-
-
+	NewMod = deepcopy(CakeMod)
+	mi = cake.Material(vp=Vn)
+	for i_ in CakeMod.elements():
+		if verb:
+			print('Old model')
+			print(i_)
+		else:
+			pass
+	zn = i_.zbot
+	lay = cake.HomogeneousLayer(zn,zn+Hn,mi)
+	NewMod.append(lay)
+	if verb:
+		print('New model')
+		print(NewMod)
+	return NewMod
 
 
 def raytrace_explicit(CakeMod,rr,Zsrc,Phase=cake.PhaseDef('p'),pps=10):
@@ -245,7 +273,49 @@ def raytrace_summary(CakeMod,rr,Zsrc,Phase=cake.PhaseDef('p'),pps=10):
 	return tt,dd,thetai
 
 
-### DIX FITTING ###
+def raytracing_NMO(CakeMod,xx,tt,Zref,dx=10,n_ref=1):
+	"""
+	Calculate data-model residuals for an up-going ray in a 1-D layered 
+	velocity structure. Uses interpolation from regularly spaced modeled
+	station location to estimate travel times at each provided station location,
+	speeding up solving of Eikonal.
+
+	:: INPUTS ::
+	:param CakeMod: pyrocko.Cake.LayeredModel with maximum depth > Zsrc
+	:param xx: station locations
+	:param tt: arrival times
+	:param Zref: guess depth to reflector
+	:param dx: model receiver spacing
+	:param n_ref: number of reflections, e.g.,
+				0 = Just up-going ray (1 leg)
+				1 = Primary reflection (single-bounce, 2 legs)
+				2 = First multiple (double-bounce, 4 legs)
+				3 = Second multiple (triple-bounce, 6 legs)
+	:: OUTPUTS ::
+	:return tt_cal: Calculated travel-travel times
+	:return dd_cal: Calculated ray-path lengths
+	:return theta_cal: Calculated incidence angle at the flat reflector
+
+	"""
+	# Create model station locations
+	xx_hat = np.arange(np.nanmin(xx),np.nanmax(xx) + dx, dx)
+	# Conduct ray-tracing for upgoing ray
+	tt_hat,dd_hat,theta_hat = raytrace_summary(CakeMod,xx_hat,Zsrc=Zref)
+	# Multiply path-lengths and travel-times for specified reflection count
+	tt_hat *= 2.*n_ref
+	dd_hat *= 2.*n_ref
+	# Conduct interpolation for datapoints
+	tt_cal = np.interp(xx,xx_hat,tt_hat)
+	dd_cal = np.interp(xx,xx_hat,dd_hat)
+	if n_ref > 0:
+		theta_cal = np.interp(xx/n_ref,xx_hat,theta_hat)
+	else:
+		theta_cal = np.interp(xx,xx_hat,theta_hat)
+
+	return tt_cal,dd_cal,theta_cal
+
+
+### GRID SEARCH FITTING FUNCTIONS ###
 
 def hyperbolic_fitting(xx,tt,Zv,Uwhb,Zwhb,Vmax=3850,dv=10):
 	"""
@@ -307,47 +377,65 @@ def hyperbolic_fitting(xx,tt,Zv,Uwhb,Zwhb,Vmax=3850,dv=10):
 	return df_out
 
 
-def raytracing_NMO(CakeMod,xx,tt,Zref,dx=10,n_ref=1):
+def raytracing_gridsearch(xx,tt,Zv,Uwhb,Zwhb,Vmax=3850,dv=10,method='incremental increase',scalar=1.3,dx=10,n_ref=1):
 	"""
-	Calculate data-model residuals for an up-going ray in a 1-D layered 
-	velocity structure. Uses interpolation from regularly spaced modeled
-	station location to estimate travel times at each provided station location,
-	speeding up solving of Eikonal.
+	Conduct a grid-search fitting for a flat reflector in a layered medium
+	where the thickness and velocity of the lowermost layer are unknowns and
+	conduct a down-sampling of the input shallow velocity structure
 
 	:: INPUTS ::
-	:param CakeMod: pyrocko.Cake.LayeredModel with maximum depth > Zsrc
-	:param xx: station locations
-	:param tt: arrival times
-	:param Zref: guess depth to reflector
-	:param dx: model receiver spacing
-	:param n_ref: number of reflections, e.g.,
-				0 = Just up-going ray (1 leg)
-				1 = Primary reflection (single-bounce, 2 legs)
-				2 = First multiple (double-bounce, 4 legs)
-				3 = Second multiple (triple-bounce, 6 legs)
-	:: OUTPUTS ::
-	:return tt_cal: Calculated travel-travel times
-	:return dd_cal: Calculated ray-path lengths
-	:return theta_cal: Calculated incidence angle at the flat reflector
+	:param xx: receiver locations
+	:param tt: reflection two-way travel times
+	:param Zv: vector of reflector depths to check
+	:param Uwhb: slowness profile from WHB analysis [in msec/m]
+	:param Zwhb: depth profile from WHB analysis [in m BGS]
+	:param Vmax: maximum velocity to consider [in m/sec]
+	:param dv: velocity increment for grid-search [in m/sec]
+	:param method: method for resample_WHB()
+	:param scalar: scalar for resample_WHB()
+	:param dx: Model station spacing for raytrace_NMO()
+	:param n_ref: Number of reflections for raytrace_NMO()
+
+	:: OUTPUT ::
+	:return df_out: pandas.DataFrame with summary of models and data-model residuals
+			Columns:
+				Z m: depth points
+				H1 m: thickness of overburden/firn layer
+				H2 m: guessed thickness of remaining ice column
+				V1rms: RMS velocity of overburden/firn layer
+				V2: guessed ice layer velocity
+				Vrms: Overall column RMS velocity
+				res L2: L-2 norm of data-model residuals for travel-times
+				res mean: Average data-model residual value
+				res std: standard deviation of data-model residuals
+				ndata: number of datapoints
 
 	"""
-	# Create model station locations
-	xx_hat = np.arange(np.nanmin(xx),np.nanmax(xx) + dx, dx)
-	# Conduct ray-tracing for upgoing ray
-	tt_hat,dd_hat,theta_hat = raytrace_summary(CakeMod,xx_hat,Zsrc=Zref)
-	# Multiply path-lengths and travel-times for specified reflection count
-	tt_hat *= 2.*n_ref
-	dd_hat *= 2.*n_ref
-	# Conduct interpolation for datapoints
-	tt_cal = np.interp(xx,xx_hat,tt_hat)
-	dd_cal = np.interp(xx,xx_hat,dd_hat)
-	if n_ref > 0:
-		theta_cal = np.interp(xx/n_ref,xx_hat,theta_hat)
-	else:
-		theta_cal = np.interp(xx,xx_hat,theta_hat)
+	Zt,Zb,uRMS = resample_WHB(Zwhb,Uwhb,method=method,scalar=scalar)
+	CM0 = generate_layercake_slow(Uv=uRMS*1e-3,Zv=[0]+list(Zb))
+	MODS = []
+	Vv = np.arange(uRMS[-1]**-1,Vmax + dv,dv)
+	# Iterate across velocities for lowermost layer
+	for V_ in Vv:
+		# Append a "half-space" to the shallow model
+		CMi = add_halfspace(CM0,V_)
+		# Iterate across reflector depths
+		for Z_ in Zv:
+			# Calculate modeled values
+			tti,ddi,thetai = raytracing_NMO(CMi,xx,tt,Z_,dx=dx,n_ref=n_ref)
+			# Calculate residuals
+			res = tt - tt_hat
+			# Get stats on residuals
+			resL2 = np.linalg.norm(res)
+			res_u = np.mean(res)
+			res_o = np.std(res)
+			# Summarize estimate
+			line = [Z_,Hsb,Z_ - Hsb,Vrms,V_,Vrmsi,resL2,res_u,res_o,len(xx)]
+			MODS.append(line)
+	df_out = pd.DataFrame(MODS,columns=['Z m','H1 m','H2 m','V1rms','V2','Vrms',\
+						  'res L2','res mean','res std','ndata'])
+	return df_out
 
-	return tt_cal,dd_cal,theta_cal
 
-
-def raytracing_gridsearch(CakeMod)
+# def raytracing_gridsearch(CakeMod):
 
