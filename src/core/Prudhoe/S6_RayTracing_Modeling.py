86a915e820fd244ec5f4537695e31cca1a7b4031
Nate Stevens <33462727+nts345045@users.noreply.github.com>
2023-03-13 13:22:13 -0400

    Update S6_RayTracing_Modeling.py

--- src/core/Prudhoe/S6_RayTracing_Modeling.py
+++ src/core/Prudhoe/S6_RayTracing_Modeling.py
@@ -1,3 +1,32 @@
 """
-:module: R
+:module: RayTracing_Modeling.py
+:purpose: Conduct ray-tracing for each pick in a given spread using the best-fit velocity
+			model from Step 5 (Sensitivity testing)
+
+:auth: Nathan T. Stevens
+:email: nts5045@psu.edu | ntstevens@wisc.edu
+
 """
+
+import sys
+import os
+import pandas as pd
+import numpy as np
+from glob import glob
+sys.path.append(os.path.join('..','..'))
+import util.Dix_1D_Raytrace_Analysis as d1d
+
+
+### MAP DATA ###
+ROOT = os.path.join('..','..','..','..','..','processed_data','Hybrid_Seismic','VelCorrected_t0','Prudhoe_Dome')
+OROOT = os.path.join(ROOT,'velocity_models')
+DPHZ = os.path.join(ROOT,'VelCorrected_Phase_Picks_O2_idsw_v5.csv')
+
+# Load pick data
+df_picks = pd.read_csv(DPHZ,parse_dates=['time']).sort_values('SRoff m')
+#
+
+
+
+
+
